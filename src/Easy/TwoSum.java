3a93ec7363e8f86511d077d57ddc644b4885be3a
Calyfs0 <bitxsugar@gmail.com>
2025-08-05 10:48:34 +0530

    Modified two sum

--- src/Easy/TwoSum.java
+++ src/Easy/TwoSum.java
@@ -1,10 +1,18 @@
 package Easy;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        int[] output = twoSum(nums, target);
+        System.out.println(Arrays.toString(output));
+    }
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> hs = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
             int pair = target - nums[i];
